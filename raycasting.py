c81050054ea96a7686aeb7b3919e1439f0ae7c10
isyahaishi-glitch <isyahaishi@gmail.com>
2026-01-10 17:28:03 +0700

    raycast WIP

--- raycasting.py
+++ raycasting.py
@@ -1,1 +1,23 @@
 import pygame as pg
+import math
+from settings import*
+
+class RayCasting :
+    def __init__(self,game):
+        self.game =game
+
+    def ray_cast(self):
+        ox,oy = self.game.player.pos
+        x_map,y_map = self.game.player.map_pos
+
+        Ray_angle = self.game.player.angle - HALF_FOV + 0.0001
+        for ray in range(NUM_RAYS):
+            sin_a = math.sin(Ray_angle)
+            cos_a = math.cos(Ray_angle)
+
+            x_vert, dx =(x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+            depth_vert = (x_vert- ox) /cos_a
+            Ray_angle += DELTA_ANGEL
+
+    def update(self):
+        self.ray_cast( )
--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import math
+
 RES = WIDTH,HEIGHT = 1366,768
 FPS = 60
 TILE =64
@@ -6,3 +8,10 @@ Player_POS = 1.5, 5
 Player_ANGLE = 0
 Player_SPEED = 0.004
 Player_ROT_SPEED = 0.002
+
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS =WIDTH //2
+HALF_NUM_RAYS = NUM_RAYS //2
+DELTA_ANGEL = FOV /NUM_RAYS
+MAX_DEPTH= 20
