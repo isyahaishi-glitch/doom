52c45b51feef78e64b8c8d5f1229da28f114284f
isyahaishi-glitch <isyahaishi@gmail.com>
2026-01-10 16:25:52 +0700

    fix pixel

--- Player.py
+++ Player.py
@@ -51,10 +51,10 @@ class Player:
 
     
     def draw(self):
-        pg.draw.line(self.game.screen, 'yellow',(self.x *100 ,self.y * 100),
-                    (self.x * 100 + WIDTH * math.cos(self.angle),
-                    self.y * 100 + WIDTH * math.sin(self.angle)),2 )
-        pg.draw.circle(self.game.screen, 'green',(self.x *100, self.y*100),15)
+        pg.draw.line(self.game.screen, 'yellow',(self.x * TILE ,self.y * TILE),
+                    (self.x * TILE + WIDTH * math.cos(self.angle),
+                    self.y * TILE + WIDTH * math.sin(self.angle)),2 )
+        pg.draw.circle(self.game.screen, 'green',(self.x *TILE, self.y*TILE),15)
 
     def update(self):
         self.movement()
