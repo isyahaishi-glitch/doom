32f62192d08712afdbcce1df377d82937e4a0ea4
isyahaishi-glitch <isyahaishi@gmail.com>
2026-01-16 14:23:23 +0700

    npc sfx + death animation

--- npc.py
+++ npc.py
@@ -1,12 +1,19 @@
 from sprite_object import *
 from random import randint,random,choice
+from collections import deque
 
 class NPC(AnimatedSprite):
     def __init__(self,game, path='resources/sprites/npc/soldier/0.png' , pos=(10.5,5.5),
                  scale =0.6,shift =0.38,animation_time =180):
         super().__init__(game,path,pos,scale,shift,animation_time)
         self.attack_image = self.get_images(self.path +"/attack")
-        self.death_image = self.get_images(self.path +"/death")
+        self.death_image = deque(
+            pg.image.load(f"{self.path}/death/death ({i}).png").convert_alpha()
+            for i in range(1, 10)
+        )
+
+
+        
         self.idle_image = self.get_images(self.path +"/idle")
         self.pain_image = self.get_images(self.path +"/pain")
         self.walk_image = self.get_images(self.path +"/walk")
@@ -19,7 +26,157 @@ class NPC(AnimatedSprite):
         self.accuracy=0.015
         self.alive =True
         self.pain = False
+        self.ray_cast_value = False
+        self.frame_counter = 0
 
     def update(self):
         self.check_animation_time()
         self.get_sprite()
+        self.run_logic()
+        # self.draw_ray_cast()
+
+    def animate_death(self):
+        if not self.alive:
+            if self.animation_trigger and self.frame_counter < len(self.death_image) - 1:
+                self.death_image.rotate(-1)
+                self.image = self.death_image[0]
+                self.frame_counter += 1
+
+                for img in self.death_image:
+                    print(img)
+
+
+
+    def animated_pain(self):
+        self.animate(self.pain_image)
+        if self.animation_trigger:
+            self.pain = False
+    def check_hit_in_npc(self):
+        if self.ray_cast_value and self.game.player.shot:
+            if HALF_WIDTH - self.sprite_half_width < self.screen_x <HALF_WIDTH + self.sprite_half_width:
+                self.game.sound.npc_pain.play()
+                self.game.player.shot = False
+                self.pain = True
+                self.health -= self.game.weapon.damage
+                self.check_health()
+
+    def check_health(self):
+        if self.health < 1:
+            self.alive = False
+            self.game.sound.npc_death.play()
+    def run_logic(self):
+        if self.alive :
+            self.ray_cast_value= self.ray_cast_player_npc()
+            self.check_hit_in_npc()
+            if self.pain :
+                self.animated_pain ()
+
+            else:
+                self.animate(self.idle_image)
+        else:
+            self.animate_death()
+
+    @property
+    def map_pos (self):
+        return int(self.x), int(self.y)
+    def ray_cast_player_npc(self):
+            if self.game.player.map_pos == self.map_pos:
+                return True
+            
+            wall_dist_v , wall_dist_h = 0,0
+            player_dist_v,player_dist_h = 0,0
+            self.ray_casting_result =[]
+            ox,oy = self.game.player.pos
+            x_map,y_map = self.game.player.map_pos
+            px = ox * TILE
+            py = oy * TILE
+
+
+            Ray_angle = self.theta
+            sin_a = math.sin(Ray_angle) or 0.000001
+            cos_a = math.cos(Ray_angle) or 0.000001
+
+            texture_hor = 1
+            texture_vert = 1
+
+            y_hor,dy =(y_map + 1, 1)if sin_a > 0 else(y_map - 1e-6, -1)
+
+            depth_hor = (y_hor - oy) /sin_a
+            x_hor = ox +depth_hor * cos_a
+
+            delta_depth = dy / sin_a
+            dx = delta_depth * cos_a
+
+            for i  in range(MAX_DEPTH):
+                tile_hor =int(x_hor), int(y_hor)
+                if tile_hor == self.map_pos:
+                    player_dist_h =depth_hor
+                    break
+                if tile_hor in self.game.map.world_map:
+                    wall_dist_h = depth_hor
+                    break
+                x_hor += dx
+                y_hor += dy
+                depth_hor += delta_depth
+
+            
+
+
+            x_vert, dx =(x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+            depth_vert = (x_vert- ox) /cos_a
+            y_vert = oy +depth_vert * sin_a
+
+            delta_depth = dx /cos_a
+            dy = delta_depth * sin_a
+
+            for i in range(MAX_DEPTH):
+                tile_vert =int(x_vert), int(y_vert)
+                if tile_vert == self.map_pos:
+                    player_dist_v = depth_vert
+                    break
+                if tile_vert in self.game.map.world_map:
+                    wall_dist_v = depth_vert
+
+                    break
+                x_vert += dx
+                y_vert += dy
+                depth_vert += delta_depth
+
+            player_dist = max(player_dist_v,player_dist_h)
+
+            wall_dist = max(wall_dist_v,wall_dist_h)
+
+            if 0 < player_dist < wall_dist or not wall_dist:
+                return True
+            return False
+    
+
+    def draw_ray_cast(self):
+        pg.draw.circle(self.game.screen,'red',(100 * self.x,100 * self.y), 15)
+        if self.ray_cast_player_npc():
+            pg.draw.line(self.game.screen, 'orange',(100 * self.game.player.x,100 * self.game.player.y),
+                         (100 * self.x , 100 * self.y), 2)
+            # if depth_vert < depth_hor:
+            #     depth ,texture = depth_vert ,texture_vert
+            #     y_vert %= 1
+            #     offset = y_vert if cos_a > 0 else ( 1 -  y_vert)
+            # else:
+            #     depth,texture = depth_hor, texture_hor
+            #     x_hor %= 1
+            #     offset = (1 - x_hor) if sin_a > 0 else x_hor
+            # ray_x = (ox + depth * cos_a) * TILE
+            # ray_y = (oy + depth * sin_a) * TILE
+
+
+            # depth *= math.cos(self.game.player.angle - Ray_angle)
+
+            # proj_height = SCREEN_DIST /(depth + 0.0001)
+
+
+            # self.ray_casting_result.append((depth,proj_height,texture,offset))
+
+            # # color = [255 /(1 + depth ** 5 * 0.00002)] * 3
+            # # pg.draw.rect(self.game.screen, color,
+            # #              (ray * SCALE, HALF_HEIGHT - proj_height //2,SCALE, proj_height))
+
+            # Ray_angle += DELTA_ANGLE
